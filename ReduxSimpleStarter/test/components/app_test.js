17ce0b6867ef69a458a05b5fddeca6bacf3d0d4b
crossmw\kmai <kmai@crossmedia.tv>
2018-01-11 11:14:48 -0500

    Add first test

---
+++ ReduxSimpleStarter/test/components/app_test.js
@@ -0,0 +1,9 @@
+import {renderComponent, expect} from '../test_helper';
+import App from '../../src/components/app';
+
+describe('App', () => {
+  it('Shows the correct text', () => {
+    const component = renderComponent(App);
+    expect(component).to.contain('React simple starter');
+  });
+});
