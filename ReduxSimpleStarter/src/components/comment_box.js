51e4e4aa6d45effdd907f2afee99268260404fd6
crossmw\kmai <kmai@crossmedia.tv>
2018-01-12 15:39:36 -0500

    TextArea event tests

--- ReduxSimpleStarter/src/components/comment_box.js
+++ ReduxSimpleStarter/src/components/comment_box.js
@@ -11,14 +11,19 @@ class CommentBox extends React.Component{
     this.setState({comment: event.target.value});
   };
 
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({comment: ''});
+  }
+
   render(){
     return(
-      <div className="comment-box">
+      <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
         <textarea 
           value={this.state.comment} 
           onChange={this.handleChange.bind(this)}/>
         <button>Submit Comment</button>
-      </div>
+      </form>
     );
   }
 }
