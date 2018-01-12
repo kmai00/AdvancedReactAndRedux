d4255cabecc3e204dfb9efff9fe95257244a6674
crossmw\kmai <kmai@crossmedia.tv>
2018-01-12 14:59:05 -0500

    Wrote more tests

--- ReduxSimpleStarter/src/components/comment_box.js
+++ ReduxSimpleStarter/src/components/comment_box.js
@@ -1,7 +1,14 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 
 class CommentBox extends React.Component{
-  
+  render(){
+    return(
+      <div className="comment-box">
+        <textarea />
+        <button>Submit Comment</button>
+      </div>
+    );
+  }
 }
 
 export default CommentBox;
